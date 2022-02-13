77597fdbb345b394e2fb6fbb77b83fd071136455
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-02-13 18:50:41 -0300

    Rendering workout input form

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
 //usamos o navigator API pra pegar as coordenadas do usuários
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -23,7 +24,7 @@ if (navigator.geolocation) {
       //pegando latitude e longitude do getCurrentPosition();
       const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+      map = L.map("map").setView(coords, 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
@@ -37,23 +38,10 @@ if (navigator.geolocation) {
       //     .openPopup();
 
       //Event listener
-
-      map.on("click", function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            //L.popup está disponível na documentação do  Leaflet
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: "running-popup",
-            })
-          )
-          .setPopupContent("Workout")
-          .openPopup();
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
       });
     },
     function () {
@@ -61,3 +49,35 @@ if (navigator.geolocation) {
     }
   );
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  //Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      "";
+  //Mostrar marca
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      //L.popup está disponível na documentação do  Leaflet
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+inputType.addEventListener("change", function () {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
